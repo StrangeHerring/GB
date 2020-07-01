2a1f22090071edf5894b6489f8a1dab99ba56807
StrangeHerring <serfangame@gmail.com>
2020-07-02 00:36:14 +0300

    1/2

--- src/Hometask2.java
+++ src/Hometask2.java
@@ -1,2 +1,12 @@
 public class Hometask2 {
+    public static void main(String[] args) {
+        byte by = 5;
+        short sh = 10;
+        int in = 100;
+        long lo = 1000;
+        float fl = 2;
+        double dou = 2.5;
+        char ch = 'a';
+        boolean bo = true;
+    }
 }
