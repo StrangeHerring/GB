8e6c9f386f6fb645643a869794082456eaf07251
StrangeHerring <serfangame@gmail.com>
2020-07-02 00:48:33 +0300

    7

---
+++ src/hometask7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class hometask7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String name = scanner.nextLine();
+        System.out.println("Привет, " + name);
+    }
+}
