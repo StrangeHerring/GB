426d5decefececa81226a38495e24e86bfe193f9
StrangeHerring <serfangame@gmail.com>
2020-07-20 00:32:49 +0300

    6

---
+++ src/Hometask6/Cat.java
@@ -0,0 +1,25 @@
+package Hometask6;
+
+public class Cat extends Animal {
+    double jumprange = 2;
+    int runrange = 200;
+    int swimrange = 0;
+    @Override
+    void jump(){
+        System.out.println("Кошка прыгнула на " + jumprange);
+    }
+    void run(){
+        System.out.println("Кошка пробежала " + runrange);
+    }
+    void swim(){
+        System.out.println("Кошка проплыла " + swimrange);
+    }
+}
+class Cat_action {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.jump();
+        cat.run();
+        cat.swim();
+    }
+}
